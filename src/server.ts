bb9fa46ce0060c3614f9d5831abcf63afbaa30ec
Evgenii Klepilin <evgenii.klepilin@mtr-design.com>
2020-05-04 15:19:30 +0600

    Added docker image for Mongo: - Fixed nodemon ts extension - Moved nodemon and jest to dev dependencies

--- src/server.ts
+++ src/server.ts
@@ -6,12 +6,83 @@ const { buildSchema } = graphql;
 
 const schema = buildSchema(`
   type Query {
-    hello: String
+    user(id: Int!): Person
+    users(shark: String): [Person]
+  },
+  type Person {
+    id: Int
+    name: String
+    age: Int
+    shark: String
+  }
+  type Mutation {
+    updateUser(id: Int!, name: String!, age: String): Person
   }
 `);
 
+interface User {
+  id: number;
+  name: string;
+  age: number;
+  shark?: string;
+}
+
+const users: User[] = [
+  {
+    id: 1,
+    name: 'Brian',
+    age: 21,
+    shark: 'Great White Shark'
+  },
+  {
+    id: 2,
+    name: 'Kim',
+    age: 22,
+    shark: 'Whale Shark'
+  },
+  {
+    id: 3,
+    name: 'Faith',
+    age: 23,
+    shark: 'Hammerhead Shark'
+  },
+  {
+    id: 4,
+    name: 'Joseph',
+    age: 23,
+    shark: 'Tiger Shark'
+  },
+  {
+    id: 5,
+    name: 'Joy',
+    age: 25,
+    shark: 'Hammerhead Shark'
+  }
+];
+
+const getUser = (args: { id: number }): User => {
+  return users.filter(user => user.id == args.id)[0];
+}
+
+const getUsers = (args: { shark?: string }): User[] => {
+  return args.shark ? users.filter(user => user.shark === args.shark) : users;
+}
+
+const updateUser = (userData: User): User => {
+  users.map(user => {
+    if (user.id === userData.id) {
+      user.name = userData.name;
+      user.age = userData.age;
+      return user;
+    }
+  });
+  return users.filter(user => user.id === userData.id)[0];
+}
+
 const root = {
-  hello: (): string => 'Hello world!'
+  user: getUser,
+  users: getUsers,
+  updateUser
 };
 
 const app: express.Application = express();
